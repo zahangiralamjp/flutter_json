cb9e3988c823cb8c951d8fa2adb3055c735eadd1
ZahangirAlamJP <thezahangir@gmail.com>
2022-02-09 13:25:20 +0600

    JSON 1st * Simple concept

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Json',
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: DefaultAssetBundle.of(context).loadString('/data.json'),
+        builder: (context, snapshot) {
+          var mydata = json.decode(snapshot.data.toString());
+          return Center(
+            child: Text(mydata['name']),
+          );
+        },
+      ),
+    );
+  }
+}
